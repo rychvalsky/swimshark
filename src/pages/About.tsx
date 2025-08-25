2e606c284a0be3494c3a0faa27ba961437352e03
rychvalsky <106488148+rychvalsky@users.noreply.github.com>
2025-08-25 19:43:52 +0200

    About: add card with SWIMSHARK Prešov description

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -6,6 +6,16 @@ export default function About(){
 
       <div className="card-grid">
         <div className="card">
+          <h3>OZ Plavecká škola SWIMSHARK Prešov</h3>
+          <p>
+            OZ Plavecká škola SWIMSHARK Prešov bola založená za účelom skvalitnenia telesného vývoja, zdravého
+            životného štýlu a pozitívneho využitia voľného času zverencov. Vedúci kurzov majú prax s výučbou a s
+            prácou s deťmi v predškolskom veku ale aj dospelými. Škola zabezpečuje komplexnú výuku plávania detí
+            všetkých vekových kategórii, prípravný kurz pre deti od 4 rokov, následne základný a zdokonaľovací kurz
+            pre deti, individuálnu výučbu a kurzy pre školy a škôlky.
+          </p>
+        </div>
+        <div className="card">
       <h3>Certifikovaní tréneri</h3>
       <p>Naši inštruktori majú certifikácie a roky učiteľskej aj pretekárskej praxe.</p>
         </div>
